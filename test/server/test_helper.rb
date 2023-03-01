bf04b336182e4d9f41f91361086bc554cfb5a68a
mishina <temma182008@gmail.com>
2023-03-01 23:39:11 +0900

    Suppress RuboCop offences

--- test/server/test_helper.rb
+++ test/server/test_helper.rb
@@ -3,15 +3,20 @@ require 'resque/server'
 
 module Resque
   module TestHelper
-    class Test::Unit::TestCase
-      include Rack::Test::Methods
-      def app
-        Resque::Server.new
-      end
+    module Test
+      module Unit
+        class TestCase
+          include Rack::Test::Methods
 
-      def self.should_respond_with_success
-        test "should respond with success" do
-          assert last_response.ok?, last_response.errors
+          def app
+            Resque::Server.new
+          end
+
+          def self.should_respond_with_success
+            test 'should respond with success' do
+              assert last_response.ok?, last_response.errors
+            end
+          end
         end
       end
     end
